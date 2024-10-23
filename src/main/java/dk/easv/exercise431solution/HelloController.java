a4c285ea5ba84ef602784bc966d35b774e447166
Henrik Boulund Meng Hansen <42587454+henrikboulund@users.noreply.github.com>
2024-10-23 14:32:57 +0200

    Implemented a reset button and validation in UI.

--- src/main/java/dk/easv/exercise431solution/HelloController.java
+++ src/main/java/dk/easv/exercise431solution/HelloController.java
@@ -3,6 +3,7 @@ package dk.easv.exercise431solution;
 import bll.ValidationLogic;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -21,21 +22,49 @@ public class HelloController {
     private ValidationLogic validationLogic = new ValidationLogic();
 
 
+    private void showMessageBox(Alert.AlertType alertType, String title, String headerText, String messageText)
+    {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(messageText);
+        alert.showAndWait();
+    }
+
     public void onBtnValidate(ActionEvent actionEvent)
     {
-        int age = Integer.parseInt(txtAge.getText());
-        boolean validation = validationLogic.validate(age);
-        lblValidationText.setVisible(true);
-
-        if(validation)
+        if(txtName.getText().isEmpty())
         {
-            lblValidationText.setText(String.format("Hello %s, your age is %s and is valid", txtName.getText(), age));
-            lblValidationText.setTextFill(Color.GREEN);
+            showMessageBox(Alert.AlertType.ERROR, "Missing information", "Name is not provided", "Your name is not entered. Please do it before validation can be done.");
+        }
+        else if(txtAge.getText().isEmpty())
+        {
+            showMessageBox(Alert.AlertType.ERROR, "Missing information", "Age is not provided", "Your age is not entered. Please do it before validation can be done.");
+        }
+        else if(!validationLogic.isNumeric(txtAge.getText()))
+        {
+            showMessageBox(Alert.AlertType.ERROR, "Input validation failed", "Age is not a number", "You have entered an age that is not a number");
         }
         else
         {
-            lblValidationText.setText(String.format("Hello %s, your age is %s and is not valid", txtName.getText(), age));
-            lblValidationText.setTextFill(Color.DARKRED);
+            int age = Integer.parseInt(txtAge.getText());
+            boolean validation = validationLogic.validate(age);
+            lblValidationText.setVisible(true);
+
+            if (validation) {
+                lblValidationText.setText(String.format("Hello %s, your age is %s and is valid", txtName.getText(), age));
+                lblValidationText.setTextFill(Color.GREEN);
+            } else {
+                lblValidationText.setText(String.format("Hello %s, your age is %s and is not valid", txtName.getText(), age));
+                lblValidationText.setTextFill(Color.DARKRED);
+            }
         }
+    }
+
+    public void onBtnReset(ActionEvent actionEvent)
+    {
+        txtName.setText("");
+        txtAge.setText("");
+        lblValidationText.setVisible(false);
     }
 }
